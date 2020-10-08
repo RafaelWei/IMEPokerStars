c9d364fe3bd76c69a1f079945a0107fdcbaa8909
pedrocarnevale <pedcarnevale@gmail.com>
2020-10-08 14:37:24 -0300

    Meu commit linha de comando

---
+++ Carta.py
@@ -0,0 +1,10 @@
+class Carta():
+    def __init__(self,valor,naipe): #construtor
+        self.valor=valor
+        self.naipe=naipe
+    def __str__(self): #para poder printar uma carta
+        return self.valor +self.naipe
+    def __repr__(self): #para poder gerar uma string que representa uma carta
+        return self.valor +self.naipe
+    def __eq__(self, other): #para poder comparar cartas
+        return self.valor==other.valor and self.naipe==other.naipe
---
+++ Mesa.py
@@ -0,0 +1,60 @@
+from Baralho import Baralho
+valores=['2','3','4','5','6','7','8','9','10','J','Q','K','A']
+naipes=["♥","♦","♠","♣"]
+def ordenar_jogadores(jogador):
+    return jogador.valor_mao
+
+class Mesa:
+    def __init__(self,qtdJogadores):
+        self.baralho = Baralho()
+        self.qtdJogadores=qtdJogadores
+        self.jogadores=[]
+        self.pot=0
+    def distribuir_cartas(self):
+        for i in range(0,2):
+            for jogador in self.jogadores:
+                jogador.adicionar_carta(self.baralho.sacar_carta())
+
+    #temos que dividir em pre flop, flop etc
+    def colocar_cartas_mesa(self):
+        for i in range(0,5):
+            carta_da_vez=self.baralho.sacar_carta()
+            for jogador in self.jogadores:
+                jogador.adicionar_carta(carta_da_vez)
+
+    def checa_vencedor(self):
+        for jogador in self.jogadores:
+            jogador.checar_melhor_mao()
+        self.jogadores.sort(key=ordenar_jogadores, reverse=True)
+        resultado=(self.jogadores[0]).valor_mao
+        empatados=[]
+        for jogador in self.jogadores:
+            if jogador.valor_mao==resultado:
+                empatados.append(jogador)
+            else:
+                break
+        if(len(empatados)>1):
+            vencedor=self.desempatar(empatados)
+        else:
+            vencedor=empatados[0]
+        print(self.jogadores)
+        return vencedor
+
+    def adicionar_jogador(self,jogador):
+        self.jogadores.append(jogador)
+
+    def desempatar(self,empatados):
+        for i in range (0,5):
+            valores_desempate=[]
+            for jogador in empatados:
+                valores_desempate.append(valores.index(jogador.mao[i].valor))
+            maximo=max(valores_desempate)
+            for jogador in empatados:
+                if valores.index(jogador.mao[i].valor)!=maximo:
+                    empatados.remove(jogador)
+            if len(empatados)==1:
+                return empatados[0]
+        return empatados
+
+
+

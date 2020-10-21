ebc10ec7fd3f94e5405cb0366849c151ad8e59b1
pedrocarnevale <42678222+pedrocarnevale@users.noreply.github.com>
2020-10-21 11:53:36 -0300

    Jogo single player quse completo

--- Botao.py
+++ Botao.py
@@ -1,16 +1,17 @@
 import pygame
 class Botao():
-    def __init__(self, texto, x, y, cor):
+    def __init__(self, texto, x, y, cor,tamanho,comprimento,altura):
         self.texto = texto
         self.x = x
         self.y = y
         self.cor = cor
-        self.comprimento = 100
-        self.altura = 50
+        self.comprimento = comprimento
+        self.altura = altura
+        self.tamanho=tamanho
 
     def desenhar(self, tela):
         pygame.draw.rect(tela, self.cor, (self.x, self.y, self.comprimento, self.altura))
-        font = pygame.font.SysFont("comicsans", 40)
+        font = pygame.font.SysFont("comicsans", self.tamanho)
         texto = font.render(self.texto, 1, (255, 255, 255))
         tela.blit(texto, (self.x + round(self.comprimento / 2) - round(texto.get_width() / 2),
                         self.y + round(self.altura / 2) - round(texto.get_height() / 2)))
